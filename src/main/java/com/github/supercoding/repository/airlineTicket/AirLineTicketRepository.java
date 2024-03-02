3ff473beb5cbf078ec0ff95446ea91b7a129c034
MangwonCassie <dudnfsla1@naver.com>
2024-03-02 14:48:31 +0900

    feat: Service에서 AirLineTicket DTO 생성 + Ticket의 getDepartAt이랑 ReturnAt 타입 안맞는 거 static 포맷터로 해결

--- src/main/java/com/github/supercoding/repository/airlineTicket/AirLineTicketRepository.java
+++ src/main/java/com/github/supercoding/repository/airlineTicket/AirLineTicketRepository.java
@@ -1,4 +1,7 @@
 package com.github.supercoding.repository.airlineTicket;
 
+import java.util.List;
+
 public interface AirLineTicketRepository {
+    List<AirlineTicket> findAllAirlineTicketsWithPlaceAndTicketType(String likePlace, String ticketType);
 }
--- src/main/java/com/github/supercoding/service/AirReservationService.java
+++ src/main/java/com/github/supercoding/service/AirReservationService.java
@@ -1,11 +1,14 @@
 package com.github.supercoding.service;
 
 import com.github.supercoding.repository.airlineTicket.AirLineTicketRepository;
+import com.github.supercoding.repository.airlineTicket.AirlineTicket;
+import com.github.supercoding.repository.users.UserEntity;
 import com.github.supercoding.repository.users.UserRepository;
 import com.github.supercoding.web.dto.airline.Ticket;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AirReservationService {
@@ -18,5 +21,14 @@ public class AirReservationService {
         //1. 유저를 userId로 가져와서, 선호하는 여행지 도출
         //2. 선호하는 여행지와 ticketType으로 AirLineTicket Table 질의해서 필요한 AirLineTicket 들고오기
         //3. 이 둘의 정보를 조합해서 Ticket DTO를 만든다.
+
+        UserEntity userEntity = userRepository.findUserById(userId);
+
+        String likePlace =  userEntity.getLikeTravelPlace();
+
+        List<AirlineTicket> airlineTickets = airLineTicketRepository.findAllAirlineTicketsWithPlaceAndTicketType(likePlace, ticketType);
+
+       List<Ticket> tickets = airlineTickets.stream().map(Ticket:: new).collect(Collectors.toList());
+        return tickets;
     }
 }
