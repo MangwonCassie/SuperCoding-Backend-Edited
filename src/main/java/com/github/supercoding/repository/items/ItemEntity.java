cd870dabb4eff32f1eea683b00ab52d5fe001aff
MangwonCassie <dudnfsla1@naver.com>
2024-03-05 12:26:07 +0900

    chore: ItemEntity @Setter, @Getter, @AllArgsConstructor, @EqualsAndHashCode() 이 부분에서 옵션도 넣어줘야함 어떤거를 hashcode와 equals의 기준으로 할 것인지 필드명을 넣어준다.

--- src/main/java/com/github/supercoding/repository/items/ItemEntity.java
+++ src/main/java/com/github/supercoding/repository/items/ItemEntity.java
@@ -1,18 +1,23 @@
 package com.github.supercoding.repository.items;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Objects;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class ItemEntity {
     private Integer id;
     private String name;
     private String type;
     private Integer price;
-
     private Integer storeId;
-
     private Integer stock;
-
-
     private String cpu;
     private String capacity;
 
@@ -26,97 +31,4 @@ public class ItemEntity {
         this.cpu = cpu;
         this.capacity = capacity;
     }
-
-    //NOTE: 모두 다 들어간 생성자
-
-
-    public ItemEntity(Integer id, String name, String type, Integer price, Integer storeId, Integer stock, String cpu, String capacity) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.price = price;
-        this.storeId = storeId;
-        this.stock = stock;
-        this.cpu = cpu;
-        this.capacity = capacity;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public String getCpu() {
-        return cpu;
-    }
-
-    public String getCapacity() {
-        return capacity;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public Integer getStoreId() {
-        return storeId;
-    }
-
-    public void setStoreId(Integer storeId) {
-        this.storeId = storeId;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
-    }
-
-    public void setCpu(String cpu) {
-        this.cpu = cpu;
-    }
-
-    public void setCapacity(String capacity) {
-        this.capacity = capacity;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ItemEntity that = (ItemEntity) o;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-
 }
