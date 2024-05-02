9e340ccbefa5e12f59800052771a7845e1d540d0
MangwonCassie <dudnfsla1@naver.com>
2024-05-02 15:20:04 +0900

    feat: JPA 쓰기 위해 dataSource1의 jpaConfig 설정

---
+++ src/main/java/com/github/supercoding/config/JpaConfig1.java
@@ -0,0 +1,36 @@
+package com.github.supercoding.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableJpaRepositories (
+        basePackages = {"com.github.supercoding.repository.items", "com.github.supercoding.repository.storeSales"}
+)
+public class JpaConfig1 {
+
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean1 (@Qualifier("dataSource1")DataSource dataSource){
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.github.supercodingspring.repository.items", "com.github.supercodingspring.repository.storeSales");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.use_sql_comment", "true");
+
+        em.setJpaPropertyMap(properties);
+        return em;
+    };
+}
