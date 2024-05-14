f7155daa68adef514cade253adfa812999ba1934
MangwonCassie <dudnfsla1@naver.com>
2024-05-14 19:55:27 +0900

    feat: itemMapperUtilTest itemEntityToItem test 로직 추가

--- src/test/java/com/github/supercoding/service/mapper/ItemMapperUtilTest.java
+++ src/test/java/com/github/supercoding/service/mapper/ItemMapperUtilTest.java
@@ -2,6 +2,8 @@ package com.github.supercoding.service.mapper;
 
 import com.github.supercoding.repository.items.ItemEntity;
 import com.github.supercoding.repository.storeSales.StoreSales;
+import com.github.supercoding.web.dto.items.Item;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,8 +28,15 @@ class ItemMapperUtilTest {
                 .storeSales(new StoreSales())
                 .build();
 
-        log.info("만들어진 item: " + item);
+        // when
+        Item item = ItemMapper.INSTANCE.itemEntityToItem(itemEntity);
 
-        //
+        //then
+        log.info("만들어진 item: " + item);
+        assertEquals(itemEntity.getPrice(), item.getPrice());
+        assertEquals(itemEntity.getId().toString(), item.getId());
+        assertEquals(itemEntity.getCapacity(), item.getSpec().getCapacity());
+        assertEquals(itemEntity.getCpu(), item.getSpec().getCpu());
+
     }
 }
