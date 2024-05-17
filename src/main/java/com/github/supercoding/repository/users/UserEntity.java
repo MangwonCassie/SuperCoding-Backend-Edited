9fc26e947bb133599bc09eb682a4a4100acc66c2
MangwonCassie <dudnfsla1@naver.com>
2024-05-17 20:00:56 +0900

    feat: chapter_100 롬복으로 코드 개선인데 (UserJdbcTemplateDao Builder부분만)

--- src/main/java/com/github/supercoding/repository/users/UserEntity.java
+++ src/main/java/com/github/supercoding/repository/users/UserEntity.java
@@ -1,14 +1,17 @@
 package com.github.supercoding.repository.users;
 
 import com.github.supercoding.repository.passenger.Passenger;
-import lombok.Builder;
+import lombok.*;
 import org.hibernate.annotations.Table;
 
 import javax.persistence.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Entity
-
 public class UserEntity {
 
     @Id
@@ -27,52 +30,6 @@ public class UserEntity {
     @OneToOne(mappedBy = "user")
     private Passenger passenger;
 
-
-
-    public UserEntity() {
-
-    }
-
-    public UserEntity(Integer userId, String userName, String likeTravelPlace, String phoneNum) {
-        this.userId = userId;
-        this.userName = userName;
-        this.likeTravelPlace = likeTravelPlace;
-        this.phoneNum = phoneNum;
-    }
-
-
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getLikeTravelPlace() {
-        return likeTravelPlace;
-    }
-
-    public void setLikeTravelPlace(String likeTravelPlace) {
-        this.likeTravelPlace = likeTravelPlace;
-    }
-
-    public String getPhoneNum() {
-        return phoneNum;
-    }
-
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
-    }
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/com/github/supercoding/repository/users/UserJdbcTemplateDao.java
+++ src/main/java/com/github/supercoding/repository/users/UserJdbcTemplateDao.java
@@ -17,12 +17,12 @@ public class UserJdbcTemplateDao implements UserRepository{
     }
 
     static RowMapper<UserEntity> userEntityRowMapper = ((rs, rowNums) ->
-            new UserEntity(
-                    rs.getInt("user_id"),
-                    rs.getNString("user_name"),
-                    rs.getNString("like_travel_place"),
-                    rs.getNString("phone_num")
-            ));
+            new UserEntity.UserEntityBuilder()
+                    .userId(rs.getInt("user_id"))
+                    .userName(rs.getNString("user_name"))
+                    .likeTravelPlace(rs.getNString("like_travel_place"))
+                    .phoneNum(rs.getNString("phone_num"))
+                    .build());
 
     @Override
     public Optional<UserEntity> findUserById(Integer userId) {
