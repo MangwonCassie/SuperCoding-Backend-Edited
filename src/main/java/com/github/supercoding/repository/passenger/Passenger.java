887e04d9892cb4824a626de18510439ab291a3ab
MangwonCassie <dudnfsla1@naver.com>
2024-05-17 19:45:00 +0900

    feat: Passenger 항공 예약 시스템 JPA 1차 개선 완료

--- src/main/java/com/github/supercoding/repository/passenger/Passenger.java
+++ src/main/java/com/github/supercoding/repository/passenger/Passenger.java
@@ -2,6 +2,7 @@ package com.github.supercoding.repository.passenger;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.util.Objects;
 
 @Getter
@@ -10,9 +11,16 @@ import java.util.Objects;
 @NoArgsConstructor
 @EqualsAndHashCode(of = "passengerId")
 @Builder
+@Entity
+@Table(name = "passenger")
 public class Passenger {
+    @Id @Column(name = "passenger_id") @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer passengerId;
+
+    @Column(name = "user_id")
     private Integer userId;
+
+    @Column(name = "passport_num", length = 50)
     private String passportNum;
 
 
